a28ff0d08143029e89cf419ae48ae5b316e4c7ca
Lucas de Souza Khenayfis <lucaskhenayfis@gmail.com>
2025-07-22 10:46:44 -0300

    Formaliza repo como pacote

--- R/mini-MH.r
+++ R/mini-MH.r
@@ -1,16 +1,12 @@
-library(data.table)
-library(arrow)
-library(argparse)
-library(lubridate)
-library(lgr)
-
-source("R/constantes.r")
-source("R/logging.r")
-source("R/utils.r")
-source("R/parser.r")
-source("R/validacao.r")
-source("R/leitura.r")
-source("R/escrita.r")
-source("R/consistencia-dados.r")
-source("R/preenchimento-dados-faltantes.r")
-source("R/predict.r")
+#' `melhor_historico_solar`
+#' 
+#' Construcao Do Melhor Historico De Insumos Observados Para Previsao De Solar Fotovoltaica
+#' 
+#' Este pacote contem as funcionalidades necessarias para tratamento de dados de geracao,
+#' cortes, irradiancia e potencia disponivel observados e posterior construcao dos melhores
+#' historicos de geracao e geracao sem cortes
+#' 
+#' @import data.table arrow argparse lgr
+#' 
+#' @keywords internal
+"_PACKAGE"
